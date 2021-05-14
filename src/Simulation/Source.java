b01adf124552167bbda4574a9fc939b87d76123e
Nicolas Perez <nicolasarturopz@gmail.com>
2021-05-14 18:38:40 +0200

    Code for drawing values from non-stationary poisson
    Still not working with the rest of things, but the code is currently there!

--- src/Simulation/Source.java
+++ src/Simulation/Source.java
@@ -1,5 +1,7 @@
 package Simulation;
 
+import java.util.Random;
+
 /**
  *	A source of products
  *	This class implements CProcess so that it can execute events.
@@ -21,6 +23,11 @@ public class Source implements CProcess
 	private double[] interarrivalTimes;
 	/** Interarrival time iterator */
 	private int interArrCnt;
+	
+	// Random number generator and its seed
+	// Should be modified to allow for different seeds, but this works
+	protected long seed = 4;
+	protected Random generator = new Random(seed);
 	
 	
 	ProductType prod;
@@ -130,4 +137,62 @@ public class Source implements CProcess
 		double res = -mean*Math.log(u);
 		return res;
 	}
+	
+	// This assumes that it'll always be with a sinusoid version
+	// Method givne follows thinning algorithm suggested here: http://www.columbia.edu/~ks20/4404-Sigman/4404-Notes-NSP.pdf
+	public double drawNonStationaryExponential(double mean, double cur_time, double amplitude) {
+		double time = turnToDayTime(cur_time);
+		
+		// The upper bound is very simple to get
+		// Just saving it in case it's needed in another weird way
+		double upperBound = mean + Math.abs(amplitude)*Math.abs(mean);
+		boolean invalid_number = true;
+		
+		double u = this.generator.nextDouble();
+		
+		double final_value = -(1.0/upperBound)*Math.log(u);
+		
+		// I guess I can transform this into a if true value later?
+		// I'll worry about it later I guess
+		while(invalid_number) {
+			
+			double validity_check = this.generator.nextDouble();
+			
+			double evaluation_at_t = evaluateSinusoidTime(mean, time, amplitude)/upperBound;
+			
+			if (validity_check <= evaluation_at_t) {
+				invalid_number = false;
+				break;
+			}
+			
+			u = this.generator.nextDouble();
+			
+			final_value = -(1.0/upperBound)*Math.log(u);
+			
+		}
+		
+		return final_value;
+	}
+	
+	/*
+	 * Temporary helper method
+	 * The objective of this is to transform a given time into what the time is related to a given day
+	 * So if the time says it's 30 hours, it'll transform it into 6 hours
+	 * Currently just divides by 24, as I'm not confident with how time is being processed
+	 * But this should be modified later on I guess
+	 * Having the method separately does make things easier
+	 */
+	public static double turnToDayTime(double time) {
+		return time/24;
+	}
+	
+	/*
+	 * I really need to understand how the time is working related to period
+	 * Because this value is just kinda lazy and assumes the time is in perfect format
+	 * Which it really isn't
+	 */
+	public static double evaluateSinusoidTime(double mean, double time, double amplitude) {
+		return mean + amplitude*Math.sin(time);
+	}
+	
 }
