3d82167903f02e2cad647d01b523c442a0dcfbe2
Pascalos99 <56164915+Pascalos99@users.noreply.github.com>
2021-05-16 00:12:18 +0200

    testing Source against Randomizer
    test results can be used in report, however, the results from Source seem to show that it may not be properly implemented, so this isn't useful in the report.
    I can however include the graphs showing randomizer against 1/rate at times t

--- src/Simulation/Source.java
+++ src/Simulation/Source.java
@@ -1,5 +1,6 @@
 package Simulation;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -194,5 +195,112 @@ public class Source implements CProcess
 	public static double evaluateSinusoidTime(double mean, double time, double amplitude) {
 		return mean + amplitude*Math.sin(time);
 	}
+	
+	/*
+	 * Extensive testing of source vs randomizer:
+	 *  - randomizer matches 1/rate(t) very closely
+	 *  - source seems to not change much over time and just matches the overal mean
+	 *  
+	 *  - I don't know what's wrong with source.. I tried fixing it but I can't find a way
+	 * 
+	public static void main(String[] args) {
+		
+		//
+		CEventList l = new CEventList();
+    	ArrayList<Queue> qlist = new ArrayList<>();
+    	qlist.add(new Queue());
+    	qlist.add(new Queue());
+    	QueueDistributor qd = new QueueDistributor(qlist);
+    	//
+		
+		Source s = new Source(qd, l, "testing");
+		Randomizer r = new Randomizer();
+		double mean = 2;
+		double amplitude = 0.8;
+		double period = 24;
+		// rate = mean + amplitude * sin( T * 2pi / period)
+		double max_T = 72;
+		int iters = 10000;
+		Randomizer.ITERATIONS = 12;
+		ArrayList<ArrayList<Double>> vals_N = new ArrayList<ArrayList<Double>>();
+		ArrayList<ArrayList<Double>> vals_P = new ArrayList<ArrayList<Double>>();
+		ArrayList<Double> times = new ArrayList<Double>();
+		double total_time_N = 0, total_time_P = 0;
+		
+		for (double T=0; T <= max_T; T += 0.5) {
+			times.add(T);
+			ArrayList<Double> list_N = new ArrayList<Double>();
+			ArrayList<Double> list_P = new ArrayList<Double>();
+			
+			long tim = System.currentTimeMillis();
+			for (int i=0; i < iters; i++)
+				list_N.add(s.drawNonStationaryExponential(mean, T, amplitude));
+			total_time_N += (System.currentTimeMillis() - tim) / 1000d;
+			
+			tim = System.currentTimeMillis();
+			for (int i=0; i < iters; i++)
+				list_P.add(r.nextNonStationaryPoisson(T, period, amplitude, mean));
+			total_time_P += (System.currentTimeMillis() - tim) / 1000d;
+			
+			vals_N.add(list_N);
+			vals_P.add(list_P);
+		}
+		
+		System.out.format("time N : %e seconds\ntime P : %e seconds\n", 
+				total_time_N / (iters * times.size()), 
+				total_time_P / (iters * times.size()));
+		
+		double total_error_N = 0;
+		double total_error_P = 0;
+		
+		ArrayList<Double> Ts = new ArrayList<>();
+		ArrayList<Double> Ns = new ArrayList<>();
+		ArrayList<Double> Ps = new ArrayList<>();
+		
+		for (int i=0; i < times.size(); i++) {
+			ArrayList<Double> N = vals_N.get(i);
+			ArrayList<Double> P = vals_P.get(i);
+			double sum_N = 0, sum_P = 0;
+			for (int j=0; j < iters; j++) {
+				sum_N += N.get(j);
+				sum_P += P.get(j); }
+			double mean_N = sum_N / iters;
+			double mean_P = sum_P / iters;
+			double mean_T = 1 / (mean + amplitude * Math.sin(times.get(i) * 2 * Math.PI / period));
+			double error_N = Math.abs(mean_T - mean_N) / mean_T;
+			double error_P = Math.abs(mean_T - mean_P) / mean_T;
+			total_error_N += error_N;
+			total_error_P += error_P;
+			Ts.add(mean_T);
+			Ns.add(mean_N);
+			Ps.add(mean_P);
+			
+			//System.out.format("%.3f; %.3f; %.3f -> %.3f; %,3f\n", mean_N, mean_P, mean_T, error_N, error_P);
+		}
+		System.out.format("error N : %e\nerror P : %e\n", 
+				total_error_N / (times.size()), 
+				total_error_P / (times.size()));
+		
+		System.out.print("Ts = ["+Ts.get(0));
+		for (int i=1; i < Ts.size(); i++)
+			System.out.print(", "+Ts.get(i));
+		System.out.println("]");
+		
+		System.out.print("Ps = ["+Ps.get(0));
+		for (int i=1; i < Ps.size(); i++)
+			System.out.print(", "+Ps.get(i));
+		System.out.println("]");
+		
+		System.out.print("Ns = ["+Ns.get(0));
+		for (int i=1; i < Ns.size(); i++)
+			System.out.print(", "+Ns.get(i));
+		System.out.println("]");
+		
+		System.out.print("times = ["+times.get(0));
+		for (int i=1; i < times.size(); i++)
+			System.out.print(", "+times.get(i));
+		System.out.println("]");
+	}
+	*/
 	
 }
